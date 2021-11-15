abb0ad5a41d4fe4e9442511e0c06d8f44c0983f6
Evandro Abreu <evandro.abreu@gmail.com>
2021-11-15 14:21:32 -0300

    completed

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,10 +3,21 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const user_id = request.header("user_id");
+
+      // console.log(request.headers);
+      // console.log(user_id);
+
+      return response
+        .status(200)
+        .json(this.listAllUsersUseCase.execute({ user_id }));
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,10 +3,17 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      return response
+        .status(200)
+        .json(this.showUserProfileUseCase.execute({ user_id }));
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
   }
 }
 
